60be32fa1b3b1f2e730dbffc1c0dda45f0ca6371
yong <gotoplay000@gmail.com>
2020-04-07 22:14:43 +0900

    Learning 5

--- src/index.js
+++ src/index.js
@@ -1,68 +1,12 @@
-import { createStore } from 'redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import store from './store';
 
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
-
-const addToDo = text => {
-  return {
-    type: ADD_TODO,
-    text
-  }
-}
-const deleteToDo = id => {
-  return {
-    type: DELETE_TODO,
-    id
-  }
-}
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
-
-const reducer = ( state=[], action ) => {
-  //console.log(action, state)
-  switch( action.type ) {
-    case ADD_TODO :
-      return [ { text: action.text , id: Date.now() }, ...state ];
-    case DELETE_TODO:
-      return state.filter( toDo => toDo.id !== action.id );
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer);
-
-store.subscribe( () => console.log(store.getState()) );
-
-const paintToDos = () => {
-  ul.innerHTML = '';
-  const toDos = store.getState();
-  toDos.forEach(toDo => {
-    const li = document.createElement('li');
-    const btn = document.createElement('button');
-    btn.innerText = 'DEL';
-    btn.addEventListener('click', dispatchDeleteToDo);
-    li.id = toDo.id;
-    li.innerText = toDo.text;
-    li.appendChild(btn);
-    ul.appendChild(li);
-  });
-}
-store.subscribe( paintToDos );
-
-const dispatchAddToDo = text => {
-  store.dispatch(addToDo(text));
-}
-const dispatchDeleteToDo = e => {
-  const id = parseInt(e.target.parentNode.id);
-  store.dispatch( deleteToDo(id) )
-}
-const onSubmint = e => {
-  e.preventDefault();
-  const toDo = input.value;
-  input.value = '';
-  dispatchAddToDo(toDo);
-}
-
-form.addEventListener('submit', onSubmint);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ src/routes/Home.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+function Home() {
+    const [ text, setText ] = useState('');
+    function onChange(e) {
+        setText(e.target.value);
+    }
+    function onSubmit(e) {
+        e.preventDefault();
+        console.log(text);
+    }
+    return (
+        <div>
+            <h1>To do</h1>
+            <form onSubmit={onSubmit}>
+                <input type="text" value={text} onChange={onChange} />
+                <button>Add</button>
+            </form>
+            <ul></ul>
+        </div>
+    );
+}
+export default Home;
